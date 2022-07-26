dd305609417604a1737f19afa188aafd1fd286f2
Ben Kehoe <ben@kehoe.io>
2022-07-26 17:23:18 -0400

    Release v0.12

--- aws_export_credentials/aws_export_credentials.py
+++ aws_export_credentials/aws_export_credentials.py
@@ -30,7 +30,7 @@ import secrets
 from botocore.session import Session
 from botocore.credentials import ReadOnlyCredentials
 
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 
 LOGGER = logging.getLogger('aws-export-credentials')
 
